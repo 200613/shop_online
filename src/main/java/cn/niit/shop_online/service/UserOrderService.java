f53622a56d07247b897921728c25ac2941f90aa2
王冉冉 <1945457312@qq.com>
2023-11-25 12:40:47 +0800

    feat:提交订单

--- src/main/java/cn/niit/shop_online/service/UserOrderService.java
+++ src/main/java/cn/niit/shop_online/service/UserOrderService.java
@@ -1,6 +1,10 @@
 package cn.niit.shop_online.service;
 
 import cn.niit.shop_online.entity.UserOrder;
+import cn.niit.shop_online.query.OrderPreQuery;
+import cn.niit.shop_online.vo.OrderDetailVO;
+import cn.niit.shop_online.vo.SubmitOrderVO;
+import cn.niit.shop_online.vo.UserOrderVO;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
@@ -8,9 +12,37 @@ import com.baomidou.mybatisplus.extension.service.IService;
  *  服务类
  * </p>
  *
- * @author 200613
+ * @author 1103386023
  * @since 2023-11-07
  */
 public interface UserOrderService extends IService<UserOrder> {
-
+Integer addGoodsOrder(UserOrderVO orderVO);
+    /**
+     * 订单详情
+     *
+     * @param id
+     * @return
+     */
+    OrderDetailVO getOrderDetail(Integer id);
+    /**
+     * 填写订单 - 获取预付订单
+     *
+     * @param userId
+     * @return
+     */
+    SubmitOrderVO getPreOrderDetail(Integer userId);
+    /**
+     * 填写订单 - 获取立即购买订单
+     *
+     * @param query
+     * @return
+     */
+    SubmitOrderVO getPreNowOrderDetail(OrderPreQuery query);
+    /**
+     * 填写订单 - 获取再次购买订单
+     *
+     * @param id
+     * @return
+     */
+    SubmitOrderVO getRepurchaseOrderDetail(Integer id);
 }
--- src/main/java/cn/niit/shop_online/service/impl/UserOrderGoodsServiceImpl.java
+++ src/main/java/cn/niit/shop_online/service/impl/UserOrderGoodsServiceImpl.java
@@ -6,15 +6,21 @@ import cn.niit.shop_online.service.UserOrderGoodsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务实现类
  * </p>
  *
- * @author 200613
+ * @author 1103386023
  * @since 2023-11-07
  */
 @Service
 public class UserOrderGoodsServiceImpl extends ServiceImpl<UserOrderGoodsMapper, UserOrderGoods> implements UserOrderGoodsService {
 
+    @Override
+    public void batchUserOrderGoods(List<UserOrderGoods> list) {
+        saveBatch(list);
+    }
 }
